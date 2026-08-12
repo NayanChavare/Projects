ed2021e083327c25f9c97bb47cf0da9b9ed60791
Nayan Yogesh Chavare <nayanyogeshchavare@Nayans-MacBook-Air.local>
2026-08-12 15:15:44 +0530

    update

---
+++ Java/test.java
@@ -0,0 +1,89 @@
+
+import java.util.*;
+
+class Main {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Array size");
+    //     int n = sc.nextInt();
+    //     int[] arr = new int[n];
+    //     for (int i = 0; i < n; i++) {
+    //         arr[i] = sc.nextInt();
+    //     }
+    //     int[] result = find(arr);
+    //     System.out.println("Odd count: " + result[0]);
+    //     System.out.println("Even count: " + result[1]);
+    //     sc.close();     
+    // }
+    // public static int find(int[] arr){
+    //     int odd = 0;
+    //     int even = 0;
+    //     for (int i=0; i<arr.length; i++){
+    //         if (arr[i]%2==0){
+    //             even++;
+    //         } else {
+    //             odd++;
+    //         }
+    //     }
+    //     return odd , even;
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     // System.out.println("Give a number: ");
+    //     // int x = sc.nextInt();
+    //     // System.out.println("Asscending");
+    //     // asscending(x);
+    //     // System.out.println("Descensding");
+    //     // descensding(x);
+    //     int sum = 0;
+    //     System.out.println("Array size");
+    //     int n = sc.nextInt();
+    //     new int[] arr;
+    //     for (int i = 0; i<n; i++){
+    //       arr[i]=sc.nextInt();
+    //     }
+    //     // System.out.println()
+    //     // int[] arr = {1,4,9};
+    //     for (int i = 0; i < arr.length; i++){
+    //       sum = sum + arr[i];
+    //     }
+    //     System.out.println(sum);
+    //     // float x = sc.nextFloat();
+    //     // float y = sc.nextFloat();
+        
+        
+    //     // System.out.println("Addition: " + add(x, y));
+    //     // System.out.println("Multiplication: " + mult(x, y));
+    //     // System.out.println("Division: " + div(x, y));
+        
+    //     sc.close();
+
+    // }
+    
+    // public static float add(float x, float y){
+    //     return x + y;
+    // }
+    
+    // public static float mult(float x, float y){
+    //     return x * y;
+    // }
+    
+    // public static float div(float x, float y){
+    //     if (y == 0) {
+    //         System.out.println("Error: Division by zero.");
+    //         return 0;
+    //     }
+    //     return x / y;
+    // }
+    // public static void asscending(int x){
+    //   for (int i=1; i<=x; i++){
+    //     System.out.println(i);
+    //   }
+    // }
+    // public static void descensding(int x){
+    //   for (int i=x; i>0; i--){
+    //     System.out.println(i);
+    //   }
+    // }
+    
+}
